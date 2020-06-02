9493edb2ffd55e92b8e12acd8cab57eca6933e8e
Je suis un dev <admin@jesuisundev.com>
2020-06-02 09:52:01 -0400

    WIP flickr

--- providers/flickr.py
+++ providers/flickr.py
@@ -2,24 +2,56 @@ from pprint import pprint as pp
 
 import requests
 
+
 def flickr_request(query):
     pp('Flickr request ...')
-    base_url = 'https://www.flickr.com/services/rest/'
-    service = 'flickr.photos.search'
-    key = ''
-    url = base_url + '?method=' + service + '&api_key=' + key + '&text=' + query + '&safe_search=3&format=json&nojsoncallback=true'
+
+    url = build_url(query)
 
     response = requests.get(url)
 
     normalized_data = normalize(response)
 
-    return response.text
+    return normalized_data
+
+
+def build_url(query):
+    base_url = 'https://www.flickr.com/services/rest/'
+    service = 'flickr.photos.search'
+    key = ''
+    url = base_url + '?method=' + service + '&api_key=' + key + '&text=' + query + '&safe_search=3&format=json&nojsoncallback=true'
+
+    return url
 
 
 def normalize(response):
-    pp(response)
-    pp(response.json())
+    normalize_data = []
+    raw_data = response.json()
+    
+    for photo in raw_data['photos']['photo']:
+        current_photo = _build_photos_url(photo)
+        normalize_data.append({
+            'name': photo['title'],
+            'photos': {
+                "thumbnail": current_photo['thumbnail'],
+                "original": current_photo['original']
+            },
+            'source': 'Flickr'
+        })
 
-    ## next up =>https://www.flickr.com/services/api/misc.urls.html
+    return normalize_data
 
-    return 'test'
+
+def _build_photos_url(photo):
+    extension = '.jpg'
+    base_url = 'https://farm%s.staticflickr.com/%s/%s_%s' % (
+        photo['farm'],
+        photo['server'],
+        photo['id'],
+        photo['secret']
+    )
+
+    return {
+        'thumbnail': base_url + '_t' + extension,
+        'original': base_url + extension
+    }
